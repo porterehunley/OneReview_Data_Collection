1a7af52ea840ff3921778f2efeb8e369080e2d1f
Porter Hunley <porterhunley@lawn-143-215-114-66.lawn.gatech.edu>
2019-08-08 20:01:17 -0400

    fixes package name

--- YTReviewsAPI/setup.py
+++ YTReviewsAPI/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 VERSION = "0.1.1"
 
 setup(
-    name='TYReviewsAPI',
+    name='YTReviewsAPI',
     version=VERSION,
     author="Porter Hunley",
     author_email="porterhunley@gatech.edu",
