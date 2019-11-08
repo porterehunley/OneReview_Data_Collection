9b5c10abd6d0a79c39857cc41266dc0af096e21f
porterehunley <porterhunley@gatech.edu>
2019-11-08 10:57:47 -0500

    release

--- YTReviewsAPI/setup.py
+++ YTReviewsAPI/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 #The packages gets all the relevant python packages, for non python files, you need to add
 # them into the manifest.in
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 setup(
     name='YTReviewsAPI',
