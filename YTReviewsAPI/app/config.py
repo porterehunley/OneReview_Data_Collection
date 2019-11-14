1acfdba461c5181796a0b0f5104bc84290a28d18
porterehunley <porterhunley@gatech.edu>
2019-11-13 19:48:09 -0500

    prep for merge

--- YTReviewsAPI/app/config.py
+++ YTReviewsAPI/app/config.py
@@ -6,4 +6,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ytreviewsapi:a@database-2.cvebpou5jr9i.us-east-2.rds.amazonaws.com/ytreviewsapi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    APP_URL = 'http://127.0.0.1:5000'   
+    APP_URL = 'http://3.210.43.88:8000'   
